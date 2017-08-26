2666ec1cda527691f06d52f764141f1054c04d70
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-08-26 15:18:06 +0900

    added more tests for std::function.

--- test/mfsm_test.cpp
+++ test/mfsm_test.cpp
@@ -30,30 +30,98 @@ struct id
     using type = T;
 };
 
+template <typename... T>
+struct type_list
+{ };
+
+
+template <typename... Args, typename L>
+std::function<void(Args...)> to_function(L l)
+{
+    return l;
+}
+
+template <typename F>
+struct extract_arg_type_list;
+
+template <typename R, typename... Args>
+struct extract_arg_type_list<std::function<R(Args...)>>
+{
+    using type = type_list<Args...>;
+};
+
+template <typename L>
+auto arg_type_list_of_impl(L l)
+{
+    return typename extract_arg_type_list<
+                        decltype(to_function(l))
+                    >::type{};
+}
+
+template <typename L>
+auto arg_type_list_of(L l)
+{
+    return arg_type_list_of_impl(l);
+}
+
+
 template <typename... Args, typename L>
 std::function<void(Args...)> return_type_of_impl(L l)
 {
     return l;
 }
 
-template <typename R, typename L>
-id<R> return_type_of(L l)
+template <typename L>
+auto return_type_of(L l)
 {
     return id<typename decltype(return_type_of_impl(l))::result_type>{};
 }
+
+
+TEST_CASE("getting the argument type list of lambda expression function", "[mfsm]")
+{
+    // compile error: lambda expression in an unevaluated operand
+    /*
+    static_assert(
+        std::is_same_v<
+                decltype(arg_type_list_of([]{})),
+                type_list<>
+        >
+    );
+    */
+
+    auto tl0 = arg_type_list_of([]{});
+    static_assert(
+        std::is_same_v<
+                decltype(tl0),
+                type_list<>
+        >
+    );
+
+    // NOTE: it seems that we can't use std::function to get the argument type list.
+    auto tl1 = arg_type_list_of([](int){});
+    static_assert(
+        std::is_same_v<
+        decltype(tl1),
+        type_list<>
+        >
+    );
+}
+
 
 TEST_CASE("getting the return type of lambda expression function", "[mfsm]")
 {
     // compile error: lambda expression in an unevaluated operand
     //decltype([](int i){ return i; }) * pL;
-
+    
     // This is OK.
     auto l = [](int i){ return i; };
     decltype(l) * pL = &l;
     CHECK((*pL)(10) == 10);
-
-    // compile error: couldn't infer template argument 'R'
-    //return_type_of([]{});
-    //return_type_of([]{ return 10; });
+    
+    return_type_of([]{});
+    return_type_of([]{ return 10; });
     return_type_of_impl([]{ return 10; });
+    
+    // NOTE: it seems that we can't use std::function to get the return type.
 }
