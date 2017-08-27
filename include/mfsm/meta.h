582fd93207636dd79c115c5fc375d822a87bae14
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-08-27 17:08:32 +0900

    added more metafunctions.

--- include/mfsm/meta.h
+++ include/mfsm/meta.h
@@ -30,6 +30,45 @@ namespace mfsm::meta
     {
         using type = T;
     };
+
+
+    template <template <typename> class F, typename TypeList>
+    struct map;
+
+    template <template <typename> class F, typename... T>
+    struct map<F, type_list<T...>>
+    {
+        using type = type_list<typename F<T>::type...>;
+    };
+
+    template <template <typename> class F, typename TypeList>
+    using map_t = typename map<F, TypeList>::type;
+
+
+    template <typename T>
+    struct void_to_void_
+    {
+        using type = T;
+    };
+
+    template <>
+    struct void_to_void_<void>
+    {
+        using type = meta::void_;
+    };
+
+
+    template <typename S, template <typename...> class T>
+    struct rename_template;
+
+    template <template <typename...> class S, typename... E, template <typename...> class T>
+    struct rename_template<S<E...>, T>
+    {
+        using type = T<E...>;
+    };
+
+    template <typename S, template <typename...> class T>
+    using rename_template_t = typename rename_template<S, T>::type;
 } // namespace mfsm::meta
 
 
